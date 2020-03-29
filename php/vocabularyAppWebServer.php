8bac27006552efc11fb62e4a3498e02ede32e9a6
Jacob Jose <jacobjose123@gmail.com>
2020-03-29 23:39:53 +0530

    Working Model, codes has been optimized

--- php/vocabularyAppWebServer.php
+++ php/vocabularyAppWebServer.php
@@ -9,23 +9,26 @@ $password = "adminJ";
 $dbname = "vocabularyapp";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
+if(isset($_GET[USER_INPUT_TYPE]) && (isset($_GET[USER_INPUT_VALUE]))) {
+    $userInputType = $_GET[USER_INPUT_TYPE];
+    $userInputValue = $_GET[USER_INPUT_VALUE];
 
-$userInputType = $_GET[USER_INPUT_TYPE];
-$userInputValue = $_GET[USER_INPUT_VALUE];
-
-if ($userInputType == USER_INPUT_TYPE_WORD) {
-    
-    $query = "SELECT * FROM allwords WHERE word LIKE '%" . $userInputValue . "%'";
+    if ($userInputType == TEXTBOX_ID_USER_INPUT_TYPE_WORD_WITH_ENTER || $userInputType == TEXTBOX_ID_USER_INPUT_TYPE_WORD_WITHOUT_ENTER){
+        
+        $query = "SELECT * FROM allwords WHERE word LIKE '%" . $userInputValue . "%'";
+        $sql = mysqli_query($conn, $query);
+    }
+    else if ($userInputType == TEXTBOX_ID_USER_INPUT_TYPE_ALL_WITH_ENTER || $userInputType == TEXTBOX_ID_USER_INPUT_TYPE_ALL_WITHOUT_ENTER){
+        
+        $query = "SELECT * FROM allwords WHERE No LIKE '%" . $userInputValue . "%' OR Word LIKE '%" . $userInputValue . "%' OR Meaning LIKE '%" . $userInputValue . "%' OR Definition LIKE '%" . $userInputValue . "%' OR Additional_Info LIKE '%" . $userInputValue . "%' OR Relevant_Example LIKE '%" . $userInputValue . "%' OR Root_Index LIKE '%" . $userInputValue . "%' OR Root_Unit LIKE '%" . $userInputValue . "%' OR Date LIKE '%" . $userInputValue . "%'";
+        $sql = mysqli_query($conn, $query);
+    }
 }
-if ($userInputType == USER_INPUT_TYPE_ALL) {
-    
-    $query = "SELECT * FROM allwords WHERE No LIKE '%" . $userInputValue . "%' OR Word LIKE '%" . $userInputValue . "%' OR Meaning LIKE '%" . $userInputValue . "%' OR Definition LIKE '%" . $userInputValue . "%' OR Additional_Info LIKE '%" . $userInputValue . "%' OR Relevant_Example LIKE '%" . $userInputValue . "%' OR Root_Index LIKE '%" . $userInputValue . "%' OR Root_Unit LIKE '%" . $userInputValue . "%' OR Date LIKE '%" . $userInputValue . "%'";
+else {
+    $query = "SELECT * FROM allwords WHERE 1";
+    $sql = mysqli_query($conn, $query);
 }
 
-
-// FETCH DATA
-
-$sql = mysqli_query($conn, $query);
 
 
 // // STORE DATA IN result VARIABLE

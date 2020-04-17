4a83f6fc262342c8ea8f4b440c2dac1872aa5cbc
Jacob Jose <jacobjose123@gmail.com>
2020-04-17 16:32:42 +0530

    Beautifying the General Help tips on Index.php

--- php/index.php
+++ php/index.php
@@ -160,7 +160,7 @@
 <!-- 	DISCLAIMER INFORMATION START -->
 	<div>
       <ul style="list-style-type:disc;">
-      <li><h3>General Help</h3></li>
+	  <h3>General Help</h3>
       <li>Enter word or characters in the 1st and 2nd TextBoxes and press 'Enter' key to search and display the words.</li>
       <li>Enter word or characters in the 3rd and 4th TextBoxes to search and display the words while you type.</li>
       <li>Simply press 'Enter' on 1st and 2nd TextBoxes WITHOUT any content to list all words.</li>
@@ -168,7 +168,7 @@
 	  </ul>
 	  
 	<ul style="list-style-type:disc;">
-      <li><h3>Hotkeys</h3></li>
+      <h3>Hotkeys</h3>
 	  <li>Toggle a checkbox of index "#" by pressing the corresponding "#" key.</li>
 	  <li>Configuration Item #10 can be toggled by pressing "1".</li>
 	  <li>Press "0" to go to the Configuration Section.</li>
@@ -184,7 +184,7 @@
 	
 	<div>
       <ul style="list-style-type:disc;">
-      <li><h3>Vocabulary App Web</h3></li>
+      <h3>Vocabulary App Web</h3>
       <li>Development started on 22-March-2020.</li>
       <li>Development completed on at 23:23 on 29-March-2020.</li>
       <li>Property of 'jacobjose123&trade;' -- All Rights Reserved.&reg;</li>

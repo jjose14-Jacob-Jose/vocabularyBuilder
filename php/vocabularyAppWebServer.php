a03802b724304a4740a3f7b27d46bb67ee1448f6
Jacob Jose <jacobjose123@gmail.com>
2020-04-24 00:11:48 +0530

    updated the SQL query to sort list by ascending order of words and descending order of dates

--- php/vocabularyAppWebServer.php
+++ php/vocabularyAppWebServer.php
@@ -17,17 +17,17 @@ if(isset($_GET[USER_INPUT_TYPE]) && (isset($_GET[USER_INPUT_VALUE]))) {
 
     if ($userInputType == TEXTBOX_ID_USER_INPUT_TYPE_WORD_WITH_ENTER || $userInputType == TEXTBOX_ID_USER_INPUT_TYPE_WORD_WITHOUT_ENTER){
         
-        $query = "SELECT * FROM allwords WHERE word LIKE '%" . $userInputValue . "%'";
+        $query = "SELECT * FROM allwords WHERE word LIKE '%" . $userInputValue . "%' ORDER BY Word ASC, Date DESC";
         $sql = mysqli_query($conn, $query);
     }
     else if ($userInputType == TEXTBOX_ID_USER_INPUT_TYPE_ALL_WITH_ENTER || $userInputType == TEXTBOX_ID_USER_INPUT_TYPE_ALL_WITHOUT_ENTER){
         
-        $query = "SELECT * FROM allwords WHERE No LIKE '%" . $userInputValue . "%' OR Word LIKE '%" . $userInputValue . "%' OR Meaning LIKE '%" . $userInputValue . "%' OR Definition LIKE '%" . $userInputValue . "%' OR Additional_Info LIKE '%" . $userInputValue . "%' OR Relevant_Example LIKE '%" . $userInputValue . "%' OR Root_Index LIKE '%" . $userInputValue . "%' OR Root_Unit LIKE '%" . $userInputValue . "%' OR Date LIKE '%" . $userInputValue . "%'";
+        $query = "SELECT * FROM allwords WHERE No LIKE '%" . $userInputValue . "%' OR Word LIKE '%" . $userInputValue . "%' OR Meaning LIKE '%" . $userInputValue . "%' OR Definition LIKE '%" . $userInputValue . "%' OR Additional_Info LIKE '%" . $userInputValue . "%' OR Relevant_Example LIKE '%" . $userInputValue . "%' OR Root_Index LIKE '%" . $userInputValue . "%' OR Root_Unit LIKE '%" . $userInputValue . "%' OR Date LIKE '% " . $userInputValue . "%' ORDER BY Word ASC, Date DESC";
         $sql = mysqli_query($conn, $query);
     }
 }
 else {
-    $query = "SELECT * FROM allwords WHERE 1";
+    $query = "SELECT * FROM allwords WHERE 1 ORDER BY Word ASC, Date DESC";
     
 	//COMMENT FOLLOWING LINE AFTER TESTING
 	//$query = "SELECT * FROM allwords WHERE 1 LIMIT 100";
